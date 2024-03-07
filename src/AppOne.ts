8308472f8100f50a460278a4caed68272546a576
Navid Dezashibi <dezashibi@gmail.com>
2024-03-07 10:29:11 +0400

    basic shapes

--- src/AppOne.ts
+++ src/AppOne.ts
@@ -41,10 +41,39 @@ const createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement)
     const scene = new BABYLON.Scene(engine);
 
     // create a camera
-    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 0, 0), scene);
+    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 0, -10), scene);
+    // add controller to camera
+    camera.attachControl(canvas, true);
 
     // create a light
     const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
+
+    // create a box
+    const box = BABYLON.MeshBuilder.CreateBox('box', {
+        size: 1,
+    }, scene);
+
+    // create a sphere
+    const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {
+        segments: 32,
+        diameter: 1,
+    }, scene);
+    sphere.position = new BABYLON.Vector3(2, 0, 0);
+
+    // create a plane
+    const plane = BABYLON.MeshBuilder.CreatePlane('plane', {}, scene);
+    plane.position = new BABYLON.Vector3(-2, 0, 0);
+    plane.rotation = new BABYLON.Vector3(45, 0, 0);
+
+    // create a line
+    const points = [
+        new BABYLON.Vector3(3, 0, 0),
+        new BABYLON.Vector3(3, 1, 1),
+        new BABYLON.Vector3(3, 1, 0),
+    ];
+    const lines = BABYLON.MeshBuilder.CreateLines('lines', {
+        points
+    }, scene);
 
     return scene;
 };
